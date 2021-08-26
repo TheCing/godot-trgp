1a91fb45e16a1f0cc3f6b05329d0bd7879012c8b
TheCing <jptyndalljr@gmail.com>
2021-08-26 11:03:08 -0400

    Init commit

---
+++ GameBoard/UnitPath.gd
@@ -0,0 +1,36 @@
+class_name UnitPath
+extends TileMap
+
+export var grid: Resource
+
+var _pathfinder: PathFinder
+var current_path := PoolVector2Array()
+
+func initialize(walkable_cells: Array):
+	_pathfinder = PathFinder.new(grid, walkable_cells)
+	
+func draw(cell_start: Vector2, cell_end: Vector2):
+	clear()
+	current_path = _pathfinder.calculate_point_path(cell_start, cell_end)
+	
+	for cell in current_path:
+		set_cellv(cell, 0)
+		
+	update_bitmask_region()
+	
+func stop():
+	_pathfinder = null
+	clear()
+
+# Debug
+#func _ready():
+#	var rect_start := Vector2(4, 4)
+#	var rect_end := Vector2(10, 8)
+#	var points := []
+#
+#	for x in rect_end.x - rect_start.x + 1:
+#		for y in rect_end.y - rect_start.y + 1:
+#			points.append(rect_start + Vector2(x, y))
+#
+#	initialize(points)
+#	draw(rect_start, Vector2(8, 7))
